35440b6b8ccc6d7fa8dca0ed15f794cc27b8eb55
Eliza Cohen <elizacohen5@gmail.com>
2024-04-25 09:00:39 -0400

    complete manipulating the DOM lecture

--- phase-1/manipulation/beginning/index.js
+++ phase-1/manipulation/beginning/index.js
@@ -18,20 +18,41 @@ Challenges
 */
 
 // ~ APIs, CRUD, `document`
+// Application Programming Interface: manupulate another data source 
+// Create, Read, Update, Delete data in external data sources
+document 
+
 
 // ~ Read/Select a node
 // -> querySelector is a DOM method that lets us look up nodes by CSS **selector**.
 // -> querySelectorAll is a DOM method that returns an array-like list of nodes that match a CSS selector.
 // -> textContent is a property of text nodes (e.g. h1, p) that contain their text.
+console.log(document.querySelector("h1")) // read by tag name 
+console.log(document.querySelector("#today")) // read by id 
+console.log(document.querySelector(".address")) // read by class
+console.log(document.querySelectorAll("p")); // read multiple elements
+console.log(document.querySelector("#today").textContent)
+const p = document.querySelector("#today")
+console.log(p.textContent)
 
 
 // ~ Update/Modify a node's attributes
 // -> To modify an attribute, just use = after the attribute name in object dot notation
 
+document.querySelector("#forever").textContent = "April 24, 2024"
+
 // ~ Delete/Remove a node
 // -> To remove an existing element, we can look it up and call the node's method `remove()`.
 
+document.querySelector("#forever").remove();
+
 // ~ Create + Append a node
+
+const newH2 = document.createElement("h2");
+// parent.append(newChild); ---> syntax for creating a new element by appending the element child as a child of the element "parent"
+newH2.textContent = "Sometime in the future..."
+document.querySelector("body").append(newH2)
+
 // -> createElement(), append()
 
 // ~ Challenges
@@ -40,7 +61,30 @@ Challenges
 //    the function should append to #dates something like:
 //        Books
 //        • The Shining
+
+function displayList(name, arr) {
+
+    const booksHeader = document.createElement("h3");
+    booksHeader.textContent = name;
+    document.querySelector("#dates").append(booksHeader);
+
+    const bookUl = document.createElement("ul");
+    document.querySelector("h3").append(bookUl);
+
+    for (let i = 0; i < arr.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = arr[i];
+        document.querySelector("ul").append(li);
+    }
+}
+
+console.log(displayList('Scary Books', ['The Shining', 'Dracula']))
+
 // 2. Replace the <strong> element with a newly created one.
 
 
+// document.querySelector("address").textContent = `by ${strongTag}`;
+
+
+ // remove and add 
 
